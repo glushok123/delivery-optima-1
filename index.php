d32cf2af08df33151e44c4355c9ff87ffdf14fdf
glushok <glushok19999@gmail.com>
2026-01-15 16:35:22 +0300

    init

--- index.php
+++ index.php
@@ -28,10 +28,10 @@ if (!$_SESSION['admin']) {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/calendar.css?v=1">
-    <link rel="stylesheet" href="css/theme.css?v=1">
-    <link rel="stylesheet" href="css/spinner.css?v=1">
-    <link rel="stylesheet" href="css/style.css?v=1">
+    <link rel="stylesheet" href="css/calendar.css?v=2">
+    <link rel="stylesheet" href="css/theme.css?v=2">
+    <link rel="stylesheet" href="css/spinner.css?v=2">
+    <link rel="stylesheet" href="css/style.css?v=2">
 </head>
 
 <body>
